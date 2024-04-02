cd8797d36c0a1aec1755f5474768ba4c82c3b785
abrazax56 <ahmadbenirusli@gmail.com>
2024-04-02 15:40:58 +0700

    prepare setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ar': ['Arab', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
