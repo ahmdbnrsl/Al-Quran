f2ce7ea7215b53d4e97902067561d281a73c71b3
abrazax56 <ahmadbenirusli@gmail.com>
2024-04-24 15:20:45 +0700

    add: toggle profile button (click event)

--- src/components/Layouts/ListSurahLayout.tsx
+++ src/components/Layouts/ListSurahLayout.tsx
@@ -1,8 +1,12 @@
 import Input from '.././Elements/Input.tsx';
-import { FaBarsProgress } from "react-icons/fa6";
+import { useState, MouseEvent } from 'react';
+import { FaCircleUser } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
 
 export default () => {
     const recentRead: boolean = false;
+    const [hidden, setHidden] = useState<boolean>(false);
+    
     return (
         <>
             <div className="hero-box">
@@ -14,7 +18,7 @@ export default () => {
                     </div>
                     :
                     <div className="history-box items-center justify-center">
-                        <h1 className="text-lg font-mulish text-zinc-300">Belum ada bacaan</h1>
+                        <h1 className="text-lg font-mulish text-teal-300 dark:text-orange-300">Belum ada bacaan</h1>
                     </div>
                 }
             </div>
@@ -26,8 +30,21 @@ export default () => {
                         identify="search"
                         />
                     </div>
-                    <button className="btn-bar">
-                        <FaBarsProgress/>
+                    <button
+                    type="button"
+                    className={`btn-bar ${hidden ? 'hidden' : 'visible'}`}
+                    onClick={(): void => {
+                        setHidden(true);
+                    }}>
+                        <FaCircleUser/>
+                    </button>
+                    <button
+                    type="button"
+                    className={`btn-bar bg-zinc-200 dark:bg-zinc-900 ${hidden ? 'visible' : 'hidden'}`}
+                    onClick={(): void => {
+                        setHidden(false);
+                    }}>
+                        <RxCross2/>
                     </button>
                 </div>
             </div>
