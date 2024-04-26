ea2ba3ab0ed97e1bc39b3e993aa06e29e2f567ba
abrazax56 <ahmadbenirusli@gmail.com>
2024-04-26 13:34:59 +0700

    add: footer

---
+++ src/components/Layouts/FooterLayout.tsx
@@ -0,0 +1,37 @@
+import DarkImage from '/logo/dark_logo.png';
+import LightImage from '/logo/light_logo.png';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                
+                <div className="flex flex-col items-center">
+                    <img
+                    src={isDarkMode ? DarkImage : LightImage}
+                    width={70}
+                    height={70}
+                    />
+                    <h1 className="font-mulishsemibold text-xl -mt-3 mb-0 border-b-0 text-zinc-800 dark:text-zinc-50">Alquranqu</h1>
+                    <p className="text-sm font-mulish text-zinc-900 dark:text-zinc-500">© 2024 | Ahmad Beni Rusli</p>
+                </div>
+                <div className="flex flex-col">
+                    <h1 className="footer-title">Navigasi</h1>
+                    <Link to="/beranda" className="footer-navigate">Beranda</Link>
+                    <Link to="/surah" className="footer-navigate">Surah</Link>
+                    <Link to="/daftar" className="footer-navigate">Daftar</Link>
+                    <Link to="/masuk" className="footer-navigate">Masuk</Link>
+                    <Link to="/faq" className="footer-navigate">FAQ</Link>
+                </div>
+                <div className="flex flex-col">
+                    <h1 className="footer-title">Informasi</h1>
+                    <Link to="/faq" className="footer-navigate">FAQ</Link>
+                    <Link to="/privasi" className="footer-navigate">Privasi</Link>
+                    <Link to="/polisi" className="footer-navigate">Polisi</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/ListSurah.tsx
+++ src/pages/ListSurah.tsx
@@ -1,9 +1,14 @@
 import Button from '.././components/Elements/Button.tsx';
 import ListSurahLayout from '.././components/Layouts/ListSurahLayout.tsx';
+import Footer from '.././components/Layouts/FooterLayout.tsx';
+
 export default () => {
     return (
-        <div className="index-box">
-          <ListSurahLayout/>
+        <div className="index-box justify-between">
+            <div className="index-box">
+                <ListSurahLayout/>
+            </div>
+            <Footer/>
         </div>
     )
 }
